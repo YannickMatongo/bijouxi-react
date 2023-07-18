820207b18fad88dd5dd2c3f679663292596f9986
Yannick Matongo <nay77127@gmail.com>
2023-07-18 13:16:25 +0200

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+// sidebar context
+import { SidebarContext } from '../contexts/SidebarContext';
+//import icons
+import {BsBag} from 'react-icons/bs'
+
+const Header = () => {
+  const {isOpen,setIsOpen} =useContext(SidebarContext);
+  return <div>
+    <div>Header</div>
+    <div onClick={()=> setIsOpen(!isOpen) } className='text-2xl relative'><BsBag className='text-2xl' /></div>
+  </div>;
+};
+
+export default Header;
